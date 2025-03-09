55e51d49c62d0b3652bbbf60baa1572155a0d5e7
AnaJ123hib <anajcrus@gmail.com>
2025-03-09 11:52:54 -0500

    Exercise resolution 2

--- Vectors/exercise-2/src/Main.java
+++ Vectors/exercise-2/src/Main.java
@@ -3,21 +3,21 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
-    static void Listado_ordenado(Object[][] listClientes, int n){
+    static void Listado_ordenado(Object[][] listClientes){
         Arrays.sort(listClientes, Comparator.comparingInt(o -> (int) o[0]));
         System.out.println("Listado ordenado: ");
         System.out.println("Edad \tTiempo\t Grado");
-        for(int i = 0; i < n ; i++){
+        for(int i = 0; i < listClientes.length ; i++){
             for (int j = 0; j < 3; j++){
                 System.out.print(listClientes[i][j] + "\t    " );
             }
             System.out.print("\n");
         }
     }
-    static  int Promedio_edad(Object[][] listClients , int n){
+    static  int Promedio_edad(Object[][] listClients){
         int count = 0;
         int sum = 0;
-        for (int i = 0; i < n ; i++){
+        for (int i = 0; i < listClients.length ; i++){
             for(int j = 0; j < 3; j++){
                 if( listClients[i][2] == "A" ){
                     count+=1;
@@ -28,25 +28,25 @@ public class Main {
         if(count == 0) return 0;
         return ((int) (sum / count));
     }
-    static int Porcentaje_4y8_malo(Object[][] listClients , int n){
+    static int Porcentaje_4y8_malo(Object[][] listClients){
         int count = 0;
 
-        for (int i = 0; i < n ; i++){
+        for (int i = 0; i < listClients.length ; i++){
             for(int j = 0; j < 3; j++){
                 if( listClients[i][1] == "C" && listClients[i][2] == "E"){
                     count+=1;
                 }
             }
         }
-        return ((int) ( count * 100 / n));
+        return ((int) ( count * 100 / listClients.length));
     }
-    static void Generar_y_Mostrar(Object[][] listClients , int n){
+    static void Generar_y_Mostrar(Object[][] listClients){
         String[] letterArray = {"A", "B" , "C", "D", "E"};
         int timeIndex;
         int gradeIndex;
         int age = 0;
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < listClients.length; i++){
             for(int j = 0; j <3; j++){
                 age = ((int) Math.round(Math.random() * 42) + 18);
                 if(j == 1){
@@ -63,10 +63,10 @@ public class Main {
             }
         }
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < listClients.length; i++){
             System.out.println("Cliente " + (i + 1));
-            System.out.println("¿Desde cuándo es cliente?" + listClients[i][1]);
-            System.out.println("¿Cuál es su grado de satisfacción con el servicio brindado?" + listClients[i][2]);
+            System.out.println("¿Desde cuándo es cliente? " + listClients[i][1]);
+            System.out.println("¿Cuál es su grado de satisfacción con el servicio brindado? " + listClients[i][2]);
             System.out.println("Edad: " + listClients[i][0]);
             System.out.print("\n");
         }
@@ -81,11 +81,11 @@ public class Main {
             if(clientsNumber > 0 && clientsNumber <= 100){
                 Object[][] listClients = new Object[clientsNumber][3];
                 System.out.println("Entonces los valores generados aleatoriamente fueron: ");
-                Generar_y_Mostrar(listClients, clientsNumber);
+                Generar_y_Mostrar(listClients);
                 System.out.println("Resultados: ");
-                System.out.println("Porcentaje de clientes entre 4 y 8 años y grado de satisfacción malo: " + Porcentaje_4y8_malo(listClients, clientsNumber) + "%");
-                System.out.println("Promedio de edad de clientes que consideran excelente el servicio: " + Promedio_edad(listClients, clientsNumber));
-                Listado_ordenado(listClients, clientsNumber);
+                System.out.println("Porcentaje de clientes entre 4 y 8 años y grado de satisfacción malo: " + Porcentaje_4y8_malo(listClients) + "%");
+                System.out.println("Promedio de edad de clientes que consideran excelente el servicio: " + Promedio_edad(listClients));
+                Listado_ordenado(listClients);
                 break;
             }
         }
